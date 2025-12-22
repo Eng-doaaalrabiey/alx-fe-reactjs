ac927ad74c4c114afb9eadf494836e7be203caef
Eng-doaaalrabiey <doaamostafa.learning@gmail.com>
2025-12-22 23:49:05 +0200

    Add components, WelcomeMessage and inline styling

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        textAlign: "center",
+        padding: "10px",
+        marginTop: "10px",
+      }}
+    >
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
